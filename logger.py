8fa73da3c526ed63ab9a35ec79e0964ec31bbc37
danny.dunn <bin_deng@intsig.net>
2019-03-20 20:23:14 +0800

    make class

--- logger.py
+++ logger.py
@@ -1,28 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env /opt/ftp_file_watcher/bin/python
 #-*- coding: utf-8 -*-
 
 # LOG_FILE = "/var/log/ftp_watcher.log"
 # LOG_FILE = "./test.log"
 
-import logging
-from logging.handlers import TimedRotatingFileHandler
-from config import LOG_FILE    #从配置文件导入日志路径配置
-
-#采用按时间日期来进行轮转
-def logger():
-    logHandler = TimedRotatingFileHandler(LOG_FILE,when="midnight")
-    # fmt = "%(asctime)s %(filename)s %(levelname)s: %(message)s"
-    fmt = "%(asctime)s %(name)s %(levelname)s: %(message)s"
-    datefmt = "%Y-%m-%d %H:%M:%S"
-    logFormatter = logging.Formatter(fmt, datefmt)
-    logHandler.setFormatter( logFormatter )
-    logger = logging.getLogger('FTP_WATCHER')
-    logger.addHandler( logHandler )
-    logger.setLevel( logging.DEBUG )
-    return logger
-    
-if __name__ == "__main__":
-    a_logger = logger()
-    for k in range(5):
-        a_logger.info("Line %d" %  k)
-        a_logger.warn("Line warn %d!" %  k)
+import logging.handlers
+ 
+class Logger(logging.Logger):
+    def __init__(self, logger_name = 'logger', filename='catch.log', rotat_when='midnight', interval = 1, backcount = 30 ):
+        super(Logger, self).__init__(self)
+        # 日志文件名
+        # if filename is None:
+        #     filename = './logs/pt.log'
+        self.name = logger_name
+        self.filename = filename
+        self.rotat_when = rotat_when
+        self.interval = interval
+        self.backcount = backcount
+ 
+        # 创建一个handler，用于写入日志文件 (每天生成1个，保留30天的日志)
+        fh = logging.handlers.TimedRotatingFileHandler(self.filename, self.rotat_when, self.interval, self.backcount)
+        fh.suffix = "%Y%m%d-%H%M.log"
+        fh.setLevel(logging.DEBUG) 
+ 
+        # 再创建一个handler，用于输出到控制台 
+        # ch = logging.StreamHandler() 
+        # ch.setLevel(logging.DEBUG) 
+ 
+        # 定义handler的输出格式 
+        # formatter = logging.Formatter('[%(asctime)s] - %(filename)s [Line:%(lineno)d] - [%(levelname)s]-[thread:%(thread)s]-[process:%(process)s] - %(message)s') 
+        fmt = "%(asctime)s %(name)s %(levelname)s: %(message)s"
+        datefmt = "%Y-%m-%d %H:%M:%S"
+        logFormatter = logging.Formatter(fmt, datefmt)
+        fh.setFormatter(logFormatter) 
+        # ch.setFormatter(logFormatter) 
+ 
+        # 给logger添加handler 
+        self.addHandler(fh) 
+        # self.addHandler(ch)
